218489bc38db32a9fa717c54308e911146e61861
Abdelmohsen Abdelgawwad <abdelmohsen@outlook.ie>
2023-01-26 20:45:32 +0000

    Project Setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <main className="App">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Counter />
         <p>
@@ -50,8 +48,8 @@ function App() {
             React Redux
           </a>
         </span>
-      </header>
-    </div>
+      </header> */}
+    </main>
   );
 }
 
