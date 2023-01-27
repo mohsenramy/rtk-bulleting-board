1bbad47f9f30c6fa94f2ba3dd14d7e9878e1e4a0
Abdelmohsen Abdelgawwad <abdelmohsen@outlook.ie>
2023-01-27 02:08:50 +0000

    Fetch Users from API

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import { Provider } from "react-redux";
 import { store } from "./app/store";
 import App from "./App";
 import "./index.css";
+import { fetchUsers } from "./features/users/usersSlice";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 
+store.dispatch(fetchUsers());
 root.render(
   <Provider store={store}>
     <App />
